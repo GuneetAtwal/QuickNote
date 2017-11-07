66cd379638413f1451a0eeb766e84d5700fc414d
GuneetAtwal <guneetatwal@gmail.com>
2017-11-08 00:58:32 +0530

    NoteViewFragment : Populate the layout with received note data
    Signed-off-by: GuneetAtwal <guneetatwal@gmail.com>

--- app/src/main/java/com/blazeworks/quicknote/NoteViewFragment.java
+++ app/src/main/java/com/blazeworks/quicknote/NoteViewFragment.java
@@ -1,11 +1,14 @@
 package com.blazeworks.quicknote;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 /**
@@ -22,8 +25,27 @@ public class NoteViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note_view, container, false);
+
+        View noteViewFragmentLayout = inflater.inflate(R.layout.fragment_note_view, container , false);
+
+        TextView noteTitle = noteViewFragmentLayout.findViewById(R.id.view_item_note_title);
+        TextView noteBody = noteViewFragmentLayout.findViewById(R.id.view_item_note_body);
+        ImageView noteImage = noteViewFragmentLayout.findViewById(R.id.view_item_note_image);
+
+        Intent intent = getActivity().getIntent();
+
+        noteTitle.setText(intent.getExtras().getString(MainActivity.NOTE_TITLE_EXTRA));
+        noteBody.setText(intent.getExtras().getString(MainActivity.NOTE_BODY_EXTRA));
+
+        /* Note.Category is passed as a serialized object so we get it as that
+         * Then we use the Note class's static method categoryToDrawable() to
+         * get the image drawable.
+         */
+        Note.Category noteCat = (Note.Category) intent.getSerializableExtra(MainActivity.NOTE_CATEGORY_EXTRA);
+        noteImage.setImageDrawable(Note.categoryToDrawable(noteCat));
+
+        /* Inflate the layout for this fragment */
+        return noteViewFragmentLayout;
     }
 
 }
