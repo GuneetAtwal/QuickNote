fde8d219ec06bd69ab364af446f8a746cfd9dc8a
GuneetAtwal <guneetatwal@gmail.com>
2017-11-08 03:04:31 +0530

    NoteDetailActivity: Add switching logic Gonna sleep now its fucking midnight again
    Signed-off-by: GuneetAtwal <guneetatwal@gmail.com>

--- app/src/main/java/com/blazeworks/quicknote/NoteDetailActivity.java
+++ app/src/main/java/com/blazeworks/quicknote/NoteDetailActivity.java
@@ -1,5 +1,6 @@
 package com.blazeworks.quicknote;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -14,10 +15,29 @@ public class NoteDetailActivity extends AppCompatActivity {
         createAndAddFragment();
     }
     private void createAndAddFragment(){
+        /* get the type of fragment */
+        Intent intent = getIntent();
+        MainActivity.FragmentToLaunch fragmentToLaunch =
+                (MainActivity.FragmentToLaunch) intent.getSerializableExtra(MainActivity.NOTE_FRAGMENT_TO_LAUNCH_EXTRA);
+
+        /* Fragment trasaction starts from here */
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        setTitle(R.string.note_view_title);
-        fragmentTransaction.add(R.id.note_container , new NoteViewFragment() , "NOTE_VIEW_FRAGMENT")
-                .commit();
+
+        /* which fragment to load is decided here */
+        switch (fragmentToLaunch){
+            case VIEW:
+                setTitle(R.string.note_view_title);
+                fragmentTransaction.add(R.id.note_container , new NoteViewFragment() , "NOTE_VIEW_FRAGMENT");
+                break;
+            case EDIT:
+                setTitle(R.string.note_edit_title);
+                fragmentTransaction.add(R.id.note_container , new NoteEditFragment() , "NOTE_EDIT_FRAGMENT");
+                break;
+        }
+        fragmentTransaction.commit();
+        /* fragment transaction ends here */
+
+
     }
 }
