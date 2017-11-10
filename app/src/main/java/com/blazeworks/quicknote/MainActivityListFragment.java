08f5f16dc6169020e758f8509ee5cffb3908723c
GuneetAtwal <guneetatwal@gmail.com>
2017-11-10 17:30:58 +0530

    MainActivityListFragment: Get notes from the database and remove the placeholder.
    Signed-off-by: GuneetAtwal <guneetatwal@gmail.com>

--- app/src/main/java/com/blazeworks/quicknote/MainActivityListFragment.java
+++ app/src/main/java/com/blazeworks/quicknote/MainActivityListFragment.java
@@ -28,11 +28,11 @@ public class MainActivityListFragment extends ListFragment {
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
 
-        notes = new ArrayList<>();
-        notes.add(new Note("Demonetisation is a fucking conspiracy" , "Modi fucked with 1.3 billion people. Demonetisation is the biggest scam ever in the history of India or the world." , Note.Category.FINANCE));
-        notes.add(new Note("Vegita still can not go SSJ3" , "I still can not believe vegita can't got SSJ3 but can go SSJB , this sucks." , Note.Category.PERSONAL));
-        notes.add(new Note("Kill it before it lays egg" , "You gotta save the universe from the abomination right." , Note.Category.QUOTE));
-        notes.add(new Note("Jio is a joke" , "4G in india can't even let you stream a 480p video smoothly. Fuck this jio bullshit." , Note.Category.TECHNICAL));
+        /* Retrieve notes from our database */
+        QuickNoteDbAdapter noteDb = new QuickNoteDbAdapter(getActivity().getBaseContext());
+        noteDb.openDatabase();
+        notes = noteDb.getAllNotes();
+        noteDb.closeDatabase();
 
         noteArrayAdapter = new NoteArrayAdapter(getActivity(),notes);
         setListAdapter(noteArrayAdapter);
