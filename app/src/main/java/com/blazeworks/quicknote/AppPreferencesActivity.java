e275606ef131963b4513f98ea902efee8f2bb0d2
GuneetAtwal <guneetatwal@gmail.com>
2017-11-10 22:46:13 +0530

    AppPreferencesActivity : Dynamically add SettingsFragment
    Signed-off-by: GuneetAtwal <guneetatwal@gmail.com>

--- app/src/main/java/com/blazeworks/quicknote/AppPreferencesActivity.java
+++ app/src/main/java/com/blazeworks/quicknote/AppPreferencesActivity.java
@@ -1,5 +1,7 @@
 package com.blazeworks.quicknote;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,5 +10,15 @@ public class AppPreferencesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    /* Creates and displays SettingsFragment */
+        addSettingsFragment();
     }
+
+    private void addSettingsFragment(){
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(android.R.id.content, new SettingsFragment() , "SETTINGS_FRAGMENT")
+                .commit();
+    }
+
 }
